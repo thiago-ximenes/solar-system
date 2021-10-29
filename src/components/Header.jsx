d9a251f74b735890797815b5526826f18ce78e40
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-10-29 17:23:20 -0300

    ✨ feat: Implementa o componete Title dentro do componente SolarSystem

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
-    
+    const header = <h1>Sistema Solar</h1>;
+
+    return (
+      <header>
+        {header}
+      </header>
+    );
   }
-
-  return (
-
-  )
 }
+
+export default Header;
