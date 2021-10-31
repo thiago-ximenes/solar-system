674ad5bad6c4919fe28f9cf22e203d3634cd3529
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-10-31 10:39:33 -0300

    ✨ Requisito 7: cria componente Missions e coloca embaixo do System Solar no app.js

---
+++ src/components/Missions.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Missions extends Component {
+  render() {
+    // const { headline } = this.props;
+
+    return (
+      <div data-testid="missions" />
+      // {/* </div> */}
+    );
+  }
+}
+
+// Missions.propTypes = {
+//   headline: PropTypes.string.isRequired,
+// };
+
+export default Missions;
