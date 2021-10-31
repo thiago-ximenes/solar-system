b462c15c8e54759bdaee397e3880aa50d14f0fe0
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-10-31 10:51:41 -0300

    ✨ Requisito 8: Implementa o componente Title dentro do componente Missions.

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import Title from './Title';
 
 class Missions extends Component {
   render() {
     // const { headline } = this.props;
 
     return (
-      <div data-testid="missions" />
-      // {/* </div> */}
+      <div data-testid="missions">
+        <Title headline="Missões" />
+      </div>
     );
   }
 }
