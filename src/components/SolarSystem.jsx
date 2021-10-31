30d2e6199e2434ffa8c9387cfbe977f3dba95322
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-10-31 10:34:00 -0300

    :sparkles: feat: Renderiza os planetas e os nomes na tela. '
    ''

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import Planets from '../data/planets';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {
+          Planets
+            .map(
+              (
+                { name, image }, i,
+              ) => <PlanetCard key={ i } planetImage={ image } planetName={ name } />,
+            )
+        }
       </div>
     );
   }
